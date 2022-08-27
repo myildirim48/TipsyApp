cb737ab2414dea37a6bf615d1dce817373ae4611
myildirim48 <muhammedyildirim48@gmail.com>
2022-08-27 22:22:07 +0300

    Controller name changed

---
+++ TipsyApp/CalculatorViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  TipsyApp
+//
+//  Created by YILDIRIM on 26.08.2022.
+//
+
+import UIKit
+
+class CalculatorViewController : UIViewController {
+    
+    @IBOutlet weak var billTextField: UITextField!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func tipChanged(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        
+    }
+    
+}
+
