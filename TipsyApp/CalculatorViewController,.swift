72ee14c2daa18b330afb5c0c318e9ed89c03e16c
Muhammed YILDIRIM <muhammedyildirim48@gmail.com>
2022-08-27 22:02:24 +0300

    Auto layout and design added

---
+++ TipsyApp/CalculatorViewController,.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  TipsyApp
+//
+//  Created by YILDIRIM on 26.08.2022.
+//
+
+import UIKit
+
+class CalculatorViewController : UIViewController {
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPrcBtn: UIButton!
+    @IBOutlet weak var tenPrcButton: UIButton!
+    @IBOutlet weak var twenrtPrccBtn: UIButton!
+    @IBOutlet weak var splitNumberLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func tipChanged(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        
+    }
+    
+}
+
